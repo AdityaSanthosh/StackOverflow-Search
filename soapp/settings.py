fa8686b34132512a5339d8db226d48e2902cf7e2
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2021-08-10 16:33:09 +0530

    allowed all_hosts

--- soapp/settings.py
+++ soapp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-u3p#ndlnt3jrlt6*x1b)p(i^6)-@wul2o!(t!i=pjm%sc3=$&s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
