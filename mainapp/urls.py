d479f75902d485d8813e4003d5df971c86c3a74c
thedarkknight <45935479+AdityaSanthosh@users.noreply.github.com>
2021-08-10 22:47:05 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Index, name='index'),
     path('search_result', views.SearchUser, name='search_result'),
-    path('', views.SearchQuery, name='search_query'),
+    path('search_result', views.SearchQuery, name='search_query'),
 ]
